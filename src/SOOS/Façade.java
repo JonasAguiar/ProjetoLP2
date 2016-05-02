798a7c263c76da31ecf1ab1ed50428c802567f50
JonasAguiar <jonas.gomes1995@gmail.com>
2016-05-02 01:03:35 -0300

    mudança façade

--- src/SOOS/Façade.java
+++ src/SOOS/Façade.java
@@ -5,7 +5,8 @@ import easyaccept.EasyAccept;
 public class Façade {
 
 	public static void main(String[] args) {
-	    args = new String[] {"meu_pacote.Facade", "diretorio_testes/usecase_1.txt", "diretorio_testes/usecase_2.txt"}; //separe cada script de teste por virgula.
+	    args = new String[] {"SOOS.Facade", "resources/usecase_1.txt", "resources/usecase_2.txt"
+	    		, "resouces/usecase_3.txt", "resources/usecase_4.txt"}; //separe cada script de teste por virgula.
 	    EasyAccept.main(args);
 	}
 }
