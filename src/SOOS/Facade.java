8ae14e55a485c2f77cf63bb2b385d397431fe5d4
JonasAguiar <jonas.gomes1995@gmail.com>
2016-05-02 01:18:43 -0300

    comit

--- src/SOOS/Facade.java
+++ src/SOOS/Facade.java
@@ -2,11 +2,11 @@ package SOOS;
 
 import easyaccept.EasyAccept;
 
-public class Façade {
+public class Facade {
 
 	public static void main(String[] args) {
 	    args = new String[] {"SOOS.Facade", "resources/usecase_1.txt", "resources/usecase_2.txt"
-	    		, "resouces/usecase_3.txt", "resources/usecase_4.txt"}; //separe cada script de teste por virgula.
+	    		, "resources/usecase_3.txt", "resources/usecase_4.txt"}; //separe cada script de teste por virgula.
 	    EasyAccept.main(args);
 	}
 }
